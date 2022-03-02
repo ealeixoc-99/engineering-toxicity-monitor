de3cedbbb50ce28f37805ec542ea7caa93402dab
Florian Marques <florian.marques@efrei.net>
2022-03-02 16:38:24 +0100

    Fix a test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -28,7 +28,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <form data-testid="form-test" onSubmit={submitSentence}>
-          <label>Write a sentence :<textarea data-testid="text-area-test" type='text' value={sentence} onChange={onChange} /></label>
+          <label data-testid="textAreaLabel">Write a sentence :<textarea data-testid="text-area-test" type='text' value={sentence} onChange={onChange} /></label>
           <input type="submit" value="Send" />
         </form>
         <h1 className="App-title">{loading === true ? (<p>Loading</p>) : response === null ? <p></p> : ( 
