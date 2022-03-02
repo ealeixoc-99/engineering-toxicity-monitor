fc32cd39cfe60a8b95dad2f34fe3d2e7d524933b
ealeixoc <enzo.aleixo-carvalho@efrei.net>
2022-03-02 22:28:37 +0100

    TEC# : Create stress test

--- backend/server_test.py
+++ backend/server_test.py
@@ -1,4 +1,5 @@
 import requests
+import time
 
 def test_get_index_status_code():
     sentence = "sentence='I hate you'"
@@ -19,3 +20,14 @@ def test_get_index_toxicity():
     sentenceToxicity = data['message']['toxicity'][0]
 
     assert sentenceToxicity > 0.9
+
+def test_stress_handle_100_requests_per_minute():
+    sentence = "sentence='I hate you'"
+    url = 'http://127.0.0.1:5000/index?' + sentence
+
+    start_time = time.time()
+    for i in range(100):
+        response = requests.get(url)
+        total_time = time.time() - start_time
+        assert total_time < 60, "Error !"
+        print("Time : " + str(total_time) + " for " + str(i+1) + " requests")
